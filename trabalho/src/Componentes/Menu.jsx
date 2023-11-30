8f43af5859ece1b3434406c68e1b6024a1efbc62
Canezin15 <jp1603207@gmail.com>
2023-11-30 10:14:07 -0400

    8º commit - tentativa de Navbar
    Tentativa de criar navbar no arquivo menu.jsx dentro da pasta componentes

---
+++ trabalho/src/Componentes/Menu.jsx
@@ -0,0 +1,32 @@
+import './Menu.css'
+
+
+export default function Menu(){
+    return(
+
+        <header>
+        
+         <nav>
+           <h1 class="logo">Central de Tutoriais DIY (Faça Você Mesmo):</h1>
+           <ul class="nav-list">
+             <li class="logo-link">Home</li>
+             <li class="logo-link">Cadastrar</li>
+             <li class="logo-link">Destaque</li>
+           </ul>
+
+           <div class="menu">
+             <span class="bar"></span>
+             <span class="bar"></span>
+             <span class="bar"></span>
+           </div>
+        
+         </nav>
+        
+        </header>
+    
+   )
+
+}
+
+
+
