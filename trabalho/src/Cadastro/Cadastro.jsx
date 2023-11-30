73ef27010cd6c0c9be0b5289fcf0742d75699fcc
Canezin15 <jp1603207@gmail.com>
2023-11-30 08:25:09 -0400

    commit 4º - Salvar
    No arquivo cadastro.jsx adicionei o localStorage e peguei do outro trabalho o "const salvar"

--- trabalho/src/Cadastro/Cadastro.jsx
+++ trabalho/src/Cadastro/Cadastro.jsx
@@ -5,15 +5,39 @@ import {useEffect, useState} from 'react'
 export default function Cadastro(){
    //Objetos
    const [titulo, setTitulo] = useState("")
-   const [descricao, setdescricao] = useState("")
+   const [descricao, setDescricao] = useState("")
    const [tipo, setTipo] = useState("")
    const [canal, setCanal] = useState("")
    const [data, setData] = useState("")
    const [link, setLink] = useState("")
-   const [Lista, setLista] = useState("")
+   const [lista, setLista] = useState("")
+   const [id, setId] = useState(  (listaLocalStorage[listaLocalStorage.length - 1]?.id + 1 || 1)  )
+   const listaLocalStorage = JSON.parse(localStorage.getItem("Lista")) || []
    
-   
-   
+   useEffect(() => { localStorage.setItem("Lista", JSON.stringify(lista)) }, [lista])
+
+
+    const salvar = (e) => {
+            e.preventDefault()
+            setLista([...lista, {
+        titulo:titulo,
+        descricao:descricao,
+        tipo:tipo,
+        canal:canal,
+        data:data,
+        link:link,
+        id:id
+    }]);
+     
+    setTitulo("")
+    setDescricao("")
+    setTipo("")
+    setCanal("")
+    setData("")
+    setLink("")
+    setId(id + 1)
+
+    }
     return(
        <h1></h1> 
     
