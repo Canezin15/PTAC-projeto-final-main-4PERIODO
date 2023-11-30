e86648100a608426186a8b934825124094936dbe
Canezin15 <jp1603207@gmail.com>
2023-11-30 07:32:31 -0400

    2º Commit - rotas e import
    Neste commit eu criei as "rotas", e seus "import"
    E em cada arquivo .jsx chamei o "export"

---
+++ trabalho/src/Cadastro/Cadastro.jsx
@@ -0,0 +1,5 @@
+export default function Cadastro(){
+    return(
+        <h1>PTAC 2 - PROJETO FINAL</h1>
+     );
+}
--- trabalho/src/main.jsx
+++ trabalho/src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
+import Cadastro from './Cadastro/Cadastro'
+import Home from './Home/Home'
+import Destaque from './Destaque/Destaque'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
       <Routes>
+        <Route path="/cadastro" element={<Cadastro />}></Route>
+        <Route path="/destaque" element={<Destaque />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
