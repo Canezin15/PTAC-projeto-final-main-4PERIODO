ccc66d1a335ccd1831ce7643e0e9fb565fa12dc8
Canezin15 <jp1603207@gmail.com>
2023-11-30 08:07:27 -0400

    commit 3º - criei os objetos
    No arquivo cadastro.jsx criei os objetos

--- trabalho/src/Cadastro/Cadastro.jsx
+++ trabalho/src/Cadastro/Cadastro.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import {useEffect, useState} from 'react'
+
+
 export default function Cadastro(){
+   //Objetos
+   const [titulo, setTitulo] = useState("")
+   const [descricao, setdescricao] = useState("")
+   const [tipo, setTipo] = useState("")
+   const [canal, setCanal] = useState("")
+   const [data, setData] = useState("")
+   const [link, setLink] = useState("")
+   const [Lista, setLista] = useState("")
+   
+   
+   
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
-     );
+       <h1></h1> 
+    
+    );
 }
